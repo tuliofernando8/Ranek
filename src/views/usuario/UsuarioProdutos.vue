e6554ba02a55bcf78238e9346463f339736d5520
tuliofernando8 <tulio-sv@hotmail.com>
2021-09-03 16:27:39 -0300

    feat: User delete

--- src/views/usuario/UsuarioProdutos.vue
+++ src/views/usuario/UsuarioProdutos.vue
@@ -7,6 +7,7 @@
       <li v-for="(produto, index) in usuario_produtos" :key="index">
         <ProdutoItem :produto="produto">
           <p>{{produto.descricao}}</p>
+          <button class="deletar" @click="deletarProduto(produto.id)"></button>
         </ProdutoItem>
       </li>
     </transition-group>
@@ -17,6 +18,7 @@
 import ProdutoAdicionar from "@/components/ProdutoAdicionar.vue";
 import ProdutoItem from "@/components/ProdutoItem.vue";
 import { mapState, mapActions } from "vuex";
+import { api } from "@/services.js";
 
 export default {
   name: "UsuarioProdutos",
@@ -29,6 +31,16 @@ export default {
   },
   methods: {
     ...mapActions(["getUsuarioProdutos"]),
+    deletarProduto(id) {
+      const confirmar = window.confirm("Deseja remover este produto");
+      if(confirmar) {
+        api.delete(`/produto/${id}`).then(() => {
+        this.getUsuarioProdutos();
+      }).catch(error => {
+        console.log(error.response);
+      })
+      }
+    }
   },
   watch: {
     login() {
@@ -55,5 +67,17 @@ h2 {
 .list-enter-active,
 .list-leave-active {
   transition: all 0.3s;
+}
+
+.deletar {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  background: url("../../assets/remove.svg") no-repeat center center;
+  width: 24px;
+  height: 24px;
+  overflow: hidden;
+  cursor: pointer;
+  border: none;
 }
 </style>
